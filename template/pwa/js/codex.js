b8378081459af0a2c985f992d2b854c734e50080
gayaganjo <gayarainbow@gmail.com>
2020-05-10 18:54:54 +0200

    app ES

--- template/pwa/js/codex.js
+++ template/pwa/js/codex.js
@@ -1,8 +1,8 @@
 //gerer les déffilement des articles de chaque élement du codex 
 function myFunction() {
 	var dots = document.getElementById("dots");
-	var moreText = document.getElementById("more");
-	var btnText = document.getElementById("myBtn");
+	var moreText = document.getElementById("more1");
+	var btnText = document.getElementById("Btn1");
 	
 
 	if (dots.style.display === "none") {
@@ -15,6 +15,26 @@ function myFunction() {
 	  moreText.style.display = "inline";
 	}
   }
+
+  function article2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("Btn2");
+    
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<img src=\'src/img/down.png\' class=\'plus'>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<img src=\'src/img/up.png' class=\'plus' >"; 
+      moreText.style.display = "inline";
+    }
+    }
+
+
+
 //gerer le déblocage des élements du codex
   var isShown = localStorage.getItem('showit');
       var element=document.getElementById('element');
