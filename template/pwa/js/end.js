7a20fde43b33530bbe75cd4cabc97a16ba2b7d4d
gayaganjo <gayarainbow@gmail.com>
2020-05-14 00:50:49 +0200

    E_S

--- template/pwa/js/end.js
+++ template/pwa/js/end.js
@@ -1,11 +1,15 @@
-const username = localStorage.getItem('username');
+const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
-
+const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 console.log(highScores);
+finalScore.innerText = mostRecentScore;
 
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
 
 saveHighScore = (e) => {
   console.log("clicked the save button");
--- template/pwa/js/highscores.js
+++ template/pwa/js/highscores.js
@@ -3,7 +3,9 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 console.log(highScores);
 
+var rang =0;
 highScoresList.innerHTML = highScores.map(score => {
-  return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  rang++;
+  return `<li class="quiz" style="font-size: 1.5rem;backdrop-filter: blur(0);line-height: 3rem;">#${rang}  |  ${score.name}  |  ${score.score}</li>`;
 })
 .join("");
